4e2c988ef24333ac1d60f4c82452ee5f9ddb085e
Nithin Deepkumar <nithindeepkumar@Nithins-MacBook-Pro.local>
2023-08-13 16:28:34 +0530

    Add tutorial 1

---
+++ basics.py
@@ -0,0 +1,62 @@
+print("Hello World!")
+
+
+a = 1
+a = 1.2
+a = True
+a = "String"
+
+
+if 4<3:
+    print("Obvious")
+else:
+    print("Not Obvious")
+
+
+# elif 
+"""elif"""
+
+
+list_ = [1,2,3,45,5]
+
+print(list_)
+
+list_.append(43)
+
+print(list_)
+
+
+
+
+for item in list_:
+    print(item)
+
+print()
+
+for i in range(len(list_)):
+    print(list_[i])
+
+
+
+
+def fun_func(param1,param2):
+    return param1 + param2
+
+a = fun_func(1,2)
+
+print(a)
+
+
+print(fun_func(1,5))
+
+print()
+
+
+import time
+
+while True:
+    print("infinite loop")
+    time.sleep(2)
+
+#https://www.github.com/nndr229
+
